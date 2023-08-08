81cf865d60d10cbf0ad9ade8a2177b1db8fc20ab
Yashwanth <blyeswanth@gmail.com>
2023-08-08 20:26:48 +0530

    code formate

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,24 +3,24 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({super.key});
 
-
-  Widget createGrid(){
+  Widget createGrid() {
     return SizedBox(
       height: 500,
-      child: GridView.count(crossAxisCount: 2,children: [
-
-      ],),
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: [],
+      ),
     );
   }
 
-
-  Widget createAppBar(){
+  Widget createAppBar() {
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0.0,
       title: const Text('good morning'),
       actions: const [
-        Padding(padding: EdgeInsets.only(right: 10),
+        Padding(
+          padding: EdgeInsets.only(right: 10),
           child: Icon(Icons.settings),
         ),
       ],
@@ -31,28 +31,21 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: (BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.blueGrey.shade300,
-            Colors.black38,
-            Colors.black38,
-            Colors.black38,
-            Colors.black38
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
-        ),
-      )
-      ),
+        gradient: LinearGradient(colors: [
+          Colors.blueGrey.shade300,
+          Colors.black38,
+          Colors.black38,
+          Colors.black38,
+          Colors.black38
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      )),
       child: Column(
         children: [
           createAppBar(),
           const SizedBox(
             height: 5,
-
           ),
           createGrid(),
-
         ],
       ),
     );
